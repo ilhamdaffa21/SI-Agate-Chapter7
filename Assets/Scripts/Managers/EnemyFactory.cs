9779d29bf83ff3c68f60f7eaaedbcc453a837d02
ilhamdaffa21 <imdaffa2001@gmail.com>
2021-09-29 23:36:49 +0700

    Tugas SI Agate Chapter 7
    Survival Shooter

---
+++ Assets/Scripts/Managers/EnemyFactory.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFactory : MonoBehaviour, IFactory
+{
+    // Start is called before the first frame update
+    [SerializeField]
+    public GameObject[] enemyPrefab;
+
+    public GameObject FactoryMethod(int tag)
+    {
+        GameObject enemy = Instantiate(enemyPrefab[tag]);
+        return enemy;
+    }
+}
